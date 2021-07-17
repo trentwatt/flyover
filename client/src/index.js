9e864b4c3c4d4fecd0977777589f4db90130d39f
trentwatt <trentwatt@hey.com>
2021-07-17 14:02:30 -0700

    added warning for brave

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,19 @@ import MobileApp from "./MobileApp"
 import "./index.css"
 import { isMobile } from "react-device-detect"
 
+navigator.brave &&
+  navigator.brave
+    .isBrave()
+    .then(
+      x =>
+        x &&
+        alert(
+          "For some reason node hit detection doesnt work in Brave. I will try to fix. Apologies."
+        )
+    )
+
+console.log()
+
 ReactDOM.render(
   <React.StrictMode>
     {isMobile ? <MobileApp /> : <DesktopApp />}
